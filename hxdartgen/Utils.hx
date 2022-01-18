69781aa3b18fd0fa4bef0b2b9e1eb31a65105c9e
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-01-18 19:02:46 +0530

    Add Custom printer for printing objects

--- hxdartgen/Utils.hx
+++ hxdartgen/Utils.hx
@@ -1,1 +1,24 @@
 package hxdartgen;
+
+import haxe.macro.Expr;
+import haxe.macro.Printer;
+
+class ObjectPrinter extends Printer {
+
+    public function new(?tabString = "\t") {
+        super(tabString);
+    }
+
+    public override function printObjectFieldKey(of:ObjectField):String {
+        return '"${of.field}"';
+    }
+
+    public override function printExpr(e:Expr):String {
+        return switch e.expr {
+            case EObjectDecl(fl):
+				"{ " + fl.map(function(fld) return printObjectField(fld)).join(", ") + " }";
+            
+            default: ""; 
+        };
+    }
+}
