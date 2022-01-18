b8ab667a90be829ea78591fb3cce059b01fbf0da
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-01-18 19:25:53 +0530

    remove unnecessary override

--- hxdartgen/Utils.hx
+++ hxdartgen/Utils.hx
@@ -12,13 +12,4 @@ class ObjectPrinter extends Printer {
     public override function printObjectFieldKey(of:ObjectField):String {
         return '"${of.field}"';
     }
-
-    public override function printExpr(e:Expr):String {
-        return switch e.expr {
-            case EObjectDecl(fl):
-				"{ " + fl.map(function(fld) return printObjectField(fld)).join(", ") + " }";
-            
-            default: ""; 
-        };
-    }
 }
